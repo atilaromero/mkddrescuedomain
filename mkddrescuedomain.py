71399a601253926618a4ee3458727d60a21adef6
Atila Romero <atilaromero@gmail.com>
2013-03-13 10:21:00 -0300

    modified:   mkddrescuedomain.py

--- mkddrescuedomain.py
+++ mkddrescuedomain.py
@@ -30,7 +30,7 @@ def listclusters(infile,offset,inode):
             if line.startswith('Type: $DATA') or line.rstrip()=='Sectors:':
                 printing=True
 
-def mkddrescue(clusters,infile,outfile,offset,clustersize=8,domain=False):
+def mkddrescue(clusters,infile,offset,clustersize=8,domain=False):
     if DEBUG:
         sys.stderr.write('Listando setores\n')
     sectors=[]
@@ -69,54 +69,67 @@ def mkddrescue(clusters,infile,outfile,offset,clustersize=8,domain=False):
             yield "%s %s +"%(x0,x1)
             pos=x0+x1
         else:
-            result="ddrescue '%s' '%s' ddrescue.log -d -i %sb -s %sb -c %s "%(infile,outfile,x[0],x[1],clustersize)
+            result="ddrescue infile outfile ddrescue.log -d -i %sb -s %sb -c %s "%(x[0],x[1],clustersize)
             yield result
 
 
-def mkscriptdir(infile,outfile,offset,inodedir,clustersize=8,domain=False,altinfile=''):
+def mkscriptdir(infile,offset,inodedir,clustersize=8,domain=False):
     clusters=[]
     for x in listinodeschildren(infile,offset,inodedir):
         for y in listclusters(infile,offset,x):
             clusters.append(y)
-    if not altinfile:
-        altinfile=infile
-    for x in mkddrescue(clusters,altinfile,outfile,offset,clustersize,domain):
+    for x in mkddrescue(clusters,infile,offset,clustersize,domain):
         print x
 
-def mkscriptonefile(infile,outfile,offset,inode,clustersize=8,domain=False,altinfile=''):
-    if not altinfile:
-        altinfile=infile
+def mkscriptonefile(infile,offset,inode,clustersize=8,domain=False):
     clusters=[x for x in listclusters(infile,offset,inode)]
-    for x in mkddrescue(clusters,altinfile,outfile,offset,clustersize,domain):
+    for x in mkddrescue(clusters,infile,offset,clustersize,domain):
         print x
 
 def main():
-    p = optparse.OptionParser(usage="""usage: %prog [options] <infile> <outfile> <sectoroffset> <inode>
-    This program creates a script with ddrescue commands to recover 
-         used areas of a damaged disk.
-    Or it can create a ddrescue domain file with those areas.""")
+    p = optparse.OptionParser(usage="""usage: %prog [options] <infile> <sectoroffset> <-f|-d> <inode>
+    This program creates a ddrescue domain file with 
+    only the used areas of a damaged disk.
+    That's usefull when you want to start by copping used areas of an unstable device.
+
+Example for a NTFS partition:
+    * /dev/sdx is an unstable device 
+    * a NTFS partition starts at sector 63
+    * MFT is at inode 0
+    * root directory is at inode 5
+
+    1-Recover the MFT: 
+      # %prog /dev/sdx 63 -f 0 > mft.domain
+      # ddrescue /dev/sdx image.dd ddrescue.log -m mft.domain
+    2-Recover the non-deleted files:
+      # %prog image.dd 63 -d 5 > allfiles.domain
+      # ddrescue /dev/sdx image.dd ddrescue.log -m allfiles.domain
+""")
     p.add_option('--clustersize', '-p', default="8",
                  help="8 para NTFS, 1 para FAT. fsstat fornece o clustersize em bytes: divida por 512")
-    p.add_option('--altinfile', '-x', default="",
-                 help="input file to be printed, if diferent from infile")
     p.add_option('--file','-f',action='store_true',default=False,
-                 help="inode pertence a um arquivo")
-    p.add_option('--dir','-d',action='store_true',default=True,
-                 help="inode pertence a um diretorio")
-    p.add_option('--domain','-m',action='store_true',default=False,
-                 help="print a domain ddrescue file, instead of a script")
+                 help="inode belongs to a file")
+    p.add_option('--dir','-d',action='store_true',default=False,
+                 help="inode belongs to a directory")
+    p.add_option('--script','-s',action='store_true',default=False,
+                 help="print a ddrescue script, insted of a domain")
     options, arguments = p.parse_args()
-    if len(arguments) < 4:
+    if len(arguments) < 3:
         p.error('missing argument')
-    if len(arguments) > 4:
+    if len(arguments) > 3:
         p.error('too many arguments')
-    infile,outfile,offset,inode=arguments
+    if not (options.dir or options.file):
+        p.error('inode is file or dir? use -f or -d to set')
+    if (options.dir and options.file):
+        p.error('-d OR -f, choose only one')
+    infile,offset,inode=arguments
     offset=int(offset)
     clustersize=int(options.clustersize)
+    domain=not options.script
     if options.file:
-        mkscriptonefile(infile,outfile,offset,inode,clustersize,options.domain,options.altinfile)
+        mkscriptonefile(infile,offset,inode,clustersize,domain)
     else:
-        mkscriptdir(infile,outfile,offset,inode,clustersize,options.domain,options.altinfile)
+        mkscriptdir(infile,offset,inode,clustersize,domain)
     
 if __name__ == '__main__':
     main()
